fad0f5726c7f238cd57a6879a2b00e435475e718
HumaionKobir01~ <humaionkobir341@gmail.com>
2023-04-15 21:07:15 +0600

    Simple form, input,change, blur, submit, preventDefault

--- src/component/Register.jsx
+++ src/component/Register.jsx
@@ -4,18 +4,25 @@ const Register = () => {
     const [email, setEmail] = useState('');
 
     const handleEmailChange = (event) => {
-        console.log(event.target.value)
+        // console.log(event.target.value)
         setEmail(event.target.value);
     }
 
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+        console.log(email, password)
+    }
+
     const handlePasswordBlur = (event) => {
-        console.log(event.target.value)
+        // console.log(event.target.value)
     }
 
     return (
         <div className='bg-fuchsia-600 p-5 rounded-lg mt-8'>
             <h4 className='text-3xl font-semibold mb-5'>Please Register</h4>
-            <form className='w-80 mx-auto'>
+            <form className='w-80 mx-auto' onSubmit={handleSubmit}>
                 <input onChange={handleEmailChange} className='mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
       focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
       disabled:bg-slate-50' type="email" name='email' placeholder='Your Email' />
